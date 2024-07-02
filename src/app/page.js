4b9341f97a9ed2978b01dff4eae09427bda383f4
Jui1 <juimandal31@gmail.com>
2024-07-02 11:08:27 +0530

    first commit

---
+++ src/app/page.js
@@ -0,0 +1,13 @@
+'use clinet'
+import Image from "next/image";
+
+// server componet
+
+//
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+     <h1>Wellcome to Next JS</h1>
+    </main>
+  );
+}
